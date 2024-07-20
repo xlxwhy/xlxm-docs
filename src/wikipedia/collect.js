cddb73ac89cd360cd41f8c4ea1baec9303dd8b3c
xlxwhy <1509868568@qq.com>
2024-07-20 21:07:54 +0800

    daily commit

--- src/wikipedia/collect.js
+++ src/wikipedia/collect.js
@@ -119,7 +119,7 @@ async function writeArticleFile(articles, year, month, day, includes) {
     let fileContentZh = getLatestNewsTitle(year, month, day)+ ` \n`;
 
     let article = null
-    let max = 10
+    let max = 20
     for (let index = 0; index < articles.length; index++) {
         if (--max < 0) break
         article = articles[index]
